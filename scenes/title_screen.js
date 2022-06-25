f692b9ce84839894796dced3e9cef91dd9d1f3d1
A.K <abdulgani.korkmaz46@gmail.com>
2022-06-25 18:48:15 +0200

    some changes

--- scenes/title_screen.js
+++ scenes/title_screen.js
@@ -6,7 +6,7 @@ class TitleScreen extends Phaser.Scene{
     preload(){
         this.load.image('start', 'assets/better_start_btn.png');
         this.load.image('start_sel', 'assets/start_selected.png');
-        this.load.image('settings', 'assets/settings_btn.png');
+        this.load.image('settings', 'assets/better_settings_btn .png');
         this.load.image('title', 'assets/title_text.png');
         
     }
@@ -14,7 +14,7 @@ class TitleScreen extends Phaser.Scene{
     create(){
         let { width, height } = this.sys.game.canvas;
         this.add.image(width/2, height/2, 'start_sel');
-        this.add.image(width/2+20, height/2+60, 'settings')
+        this.add.image(width/2, height/2+80, 'settings')
         this.add.image(width/2-20, height/2-20, 'title')
     }
 
