318aae0f93a6f6dc47bbd21b3064d4a7857f0f95
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2022-06-27 14:59:57 +0200

    cursor

--- scenes/title_screen.js
+++ scenes/title_screen.js
@@ -1,40 +1,55 @@
-class TitleScreen extends Phaser.Scene{
-    constructor(){
-        super('TitleScreen')
+class TitleScreen extends Phaser.Scene {
+  constructor() {
+    super("TitleScreen");
+    this.cursor = 0;
+  }
+
+  preload() {
+    this.load.image("start", "assets/better_start_btn.png");
+    this.load.image("start_sel", "assets/start_selected.png");
+    this.load.image("settings", "assets/better_settings_btn .png");
+    this.load.image("title", "assets/title_text.png");
+    this.load.image("select_block", "assets/select_block.png");
+  }
+
+  create() {
+    let { width, height } = this.sys.game.canvas;
+    this.add.image(width / 2, height / 2, "start");
+    this.add.image(width / 2, height / 2 + 80, "settings");
+    this.add.image(width / 2 - 20, height / 2 - 20, "title");
+    this.my_marker = this.add.sprite(
+      width / 2 - 5,
+      height / 2 + 10,
+      "select_block"
+    );
+  }
+
+  up_cursor() {
+    if (this.cursor < 1) this.cursor++;
+  }
+
+  down_cursor() {
+    if (this.cursor > 0) this.cursor--;
+  }
+
+  update() {
+    // key objects
+    let keyobj_up = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.UP
+    );
+    let keyobj_down = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
+    );
+
+    // checking for press and calculating cursor
+    if (keyobj_up.isDown) {
+      this.up_cursor();
+      window.alert(this.cursor);
     }
 
-    preload(){
-        this.load.image('start', 'assets/better_start_btn.png');
-        this.load.image('start_sel', 'assets/start_selected.png');
-        this.load.image('settings', 'assets/better_settings_btn .png');
-        this.load.image('title', 'assets/title_text.png');
-        
+    if (keyobj_down.isDown) {
+      this.down_cursor();
+      window.alert(this.cursor);
     }
-
-    cursor_fun(event){
-        console.log('You clicked');
-
-    }
-
-    create(){
-        let { width, height } = this.sys.game.canvas;
-        this.add.image(width/2, height/2, 'start');
-        this.add.image(width/2, height/2+80, 'settings')
-        this.add.image(width/2-20, height/2-20, 'title')
-        this.input.keyboard.on('keyup_UP', this.cursor_fun, this)
-        
-    }
-
-    update(){
-        let keyobj = this.input.keyboard.addKey('W');
-        if (keyobj.isDown){
-            console.log('Key is Pressed')
-        }
-       
-
-    }
-
-
-    
+  }
 }
-
